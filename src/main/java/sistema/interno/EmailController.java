fd3941322ad108f9b31188fe269685fcdc812c9d
Merkado13 <marketyt@live.com>
2020-03-24 17:11:13 +0100

    Add Concurrency

--- src/main/java/sistema/interno/EmailController.java
+++ src/main/java/sistema/interno/EmailController.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -42,6 +44,8 @@ public class EmailController {
 	public static final String WELCOME_SUBJECT = "Welcome";
 	public static final String NEW_BOOK_SUBJECT = "Nueva Publicacion!";
 	
+	private final int NUM_THREADS = 20;
+	private ExecutorService pool = Executors.newFixedThreadPool(NUM_THREADS);
 	
 	@RequestMapping(value = "/sendemail")
 	public String sendEmail() throws AddressException, MessagingException, IOException {
@@ -50,26 +54,43 @@ public class EmailController {
 	}
 	
 	@RequestMapping(value = "/welcomeemail")
-	public String welcomeEmail(@RequestBody String email) throws AddressException, MessagingException, IOException {
+	public String welcomeEmail(@RequestBody String email) {
 		
-	   emailSender.sendEmail(email, WELCOME_SUBJECT, welcome);
-	   return "Email sent successfully";
+	   pool.execute(() -> welcomedEmail(email));
+	   return "Sending welcome email...";
 	}
 	
 	
 	@RequestMapping(value = "/newbookemail")	
-	public String welcomeEmail(@RequestBody Map<String, String> emailData) throws AddressException, MessagingException, IOException {
+	public String welcomeEmail(@RequestBody Map<String, String> emailData) {
+		
+		pool.execute(() -> newBookEmail(emailData));
+		return "Sending emails...";
+	}
+	
+	private void welcomedEmail(String email) {
+		try {
+			emailSender.sendEmail(email, WELCOME_SUBJECT, welcome);
+		} catch (MessagingException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void newBookEmail(Map<String, String> emailData) {
 		String bodyMessage = emailSender.getNewBookEmailBodyMessage(emailData.get("authorName"), emailData.get("title"));
 		
 		String [] userEmails = emailData.get("userEmails").split(";");
 		for(String email : userEmails) {
 			System.out.println("Enviando email a: "+ email);
-			emailSender.sendEmail(email, NEW_BOOK_SUBJECT, bodyMessage);
+			try {
+				emailSender.sendEmail(email, NEW_BOOK_SUBJECT, bodyMessage);
+			} catch (MessagingException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-		
-		return "Email sent successfully";
 	}
-	
 	/*@RequestMapping(value = "/newbookemail")
 	public String welcomeEmail(@RequestBody NewBookEmailData emailData) throws AddressException, MessagingException, IOException {
 		System.out.println("Hola soy el servidor");
